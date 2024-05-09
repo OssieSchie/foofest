8003fa880d2952110ec02d5d5a192a40efbb1dd7
Gustav <gubo0001@stud.kea.dk>
2024-05-09 09:40:45 +0200

    feat: Add API functions for reserving and completing spot reservations

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -22,3 +22,46 @@ export async function getAllAvailableSpots() {
   const res = await fetch(`${rootUrl}/available-spots`);
   return await res.json();
 }
+
+export async function reserveSpot(areaInput, amountInput) {
+  let headersList = {
+    Accept: "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    area: areaInput,
+    amount: amountInput,
+  });
+
+  let response = await fetch(`${rootUrl}/reserve-spot`, {
+    method: "PUT",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  return data;
+}
+
+export async function completeReservation(id) {
+  let headersList = {
+    Accept: "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    id: id,
+  });
+
+  let response = await fetch(`${rootUrl}/fullfill-reservation`, {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  return data;
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import { completeReservation, getAllBands, reserveSpot } from "@/app/lib/api";
 
-export default function Home() {
+export default async function Home() {
+  //Api Testing Functions and CLG
+  // const data1 = await getAllBands();
+  // const data2 = await reserveSpot("Helheim", 1);
+  // console.log(data2);
+  // const data3 = await completeReservation(data2.id);
+  // console.log(data3);
+
   return <main></main>;
 }
