9cbbc0ca5421634b0b414f7bb078eea30318de1a
Gustav <gubo0001@stud.kea.dk>
2024-05-09 12:11:14 +0200

    chore: Update band page to display band name and genre

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 async function bandPage({ params }) {
   const { slug } = params;
-
   const data = await getBandsBySlug(slug);
 
-  console.log(data.name);
+  console.log(data);
 
   return (
     <div>
-      <h1>Bands navn: {data.slug}</h1>
+      <h1>Bands navn: {data.name}</h1>
+      <h3>Genre: {data.genre}</h3>
     </div>
   );
 }
