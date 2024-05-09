5633653902c1f0b6e7c8674027169b52df191d24
OssieSchie <osvald.rye@gmail.com>
2024-05-09 14:40:26 +0200

    jabba dabba doo

---
+++ src/app/components/Stages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Stages() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Stages
--- src/app/stages/page.jsx
+++ src/app/stages/page.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { getAllSchedule } from "@/app/lib/api";
+import Stages from '../components/Stages';
 
-function stages() {
-    const data = getAllSchedule();
+export default async function stages() {
+    const data = await getAllSchedule();
     console.log(data);
+
+    const Midgard = data.Midgard;
+    const Vanaheim = data.Vanaheim;
+    const Jotunheim = data.Jotunheim;
+
+    const text = data.toString();
     
   return (
     <main>
-      <p>Hello kan du se mig??</p>
+      <h1>Stages</h1>
+
+      <p>{text}</p>
+      {/* <div>
+        <Stages data={Midgard}/>
+        <Stages data={Vanaheim}/>
+        <Stages data={data.Jotunheim}/>
+      </div> */}
+     
     </main>
   )
 }
 
-export default stages
